6069a3638de1f8a6fcfdeab8ff26f943fe81cc02
vennapoeg <esbe.lacroix@gmail.com>
2023-01-13 11:03:16 +0200

    Kalkulaator

---
+++ Kalkulaator/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Kalkulaator;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Kalkulaator");
+        Console.WriteLine("Palun sisesta esimene arv");
+        float firstNr = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Palun sisesta tehe: ");
+        string operation = Console.ReadLine();
+
+        Console.WriteLine("Palun sisesta teine arv");
+        float secondNr = int.Parse(Console.ReadLine());
+
+
+
+        try
+        {
+            switch (operation)
+            {
+                case "+":
+                    float resultAdd = firstNr + secondNr;
+                    Console.WriteLine("Kahe numbri liitmisel on vastus: " + resultAdd);
+                    break;
+
+                case "-":
+                    float resultsub = firstNr - secondNr;
+                    Console.WriteLine("Kahe numbri lahutamisel on vastus: " + resultsub);
+                    break;
+
+                case "*":
+                    float resultmult = firstNr * secondNr;
+                    Console.WriteLine("Kahe numbri korrutamisel on vastus: " + resultmult);
+                    break;
+
+                case "/":
+                    float resultdiv = firstNr / secondNr;
+                    Console.WriteLine("Kahe numbri jagamisel on vastus: " + resultdiv);
+                    break;
+                            
+                default:
+                    break;
+
+            }
+        }
+        catch (FormatException)//numbrilise v''rtuse kontroll
+        {
+            Console.WriteLine("Vale numbri formaat. Kas sa ei saa siis aru.");
+            //throw;
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("ERROR. Sisestatud number on liiga suur.");
+        }
+    }
+}
